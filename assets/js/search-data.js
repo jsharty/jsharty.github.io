76c26220781c59f8d15271f52c6ac72c8e32bf68
jsharty <63143027+jsharty@users.noreply.github.com>
2025-08-23 04:24:38 +0000

    Deploying to gh-pages from @ jsharty/jsharty.github.io@783871a10491a7c6cf5b26f2b47f1fdcb8b617bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6A%75%73%74%69%6E%68%61%72%74%79@%61%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +515,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=uPm7xaoAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
